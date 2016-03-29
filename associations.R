a8ca5f9557c91c47b4b6954233e0b0f27b18f722
Alejandro Damian Serrano <athorybia@alejandros-mbp.devices.brown.edu>
2016-03-29 18:37:32 -0400

    host 18S aligned

--- associations.R
+++ associations.R
@@ -1,11 +1,12 @@
 ##Hyperiid-Host Associations
-setwd("Documents/0BIOL_Phylogenetics/phylobio_final_project")
+setwd("~/Documents/0BIOL_Phylogenetics/phylobio_final_project")
 ass = unique(read.table("Associations.csv", sep = '\t', header = T)[,c(1,2)])
 amphipods = sort(as.vector(unique(ass$amphipod)))
 hosts = sort(as.vector(unique(ass$host)))
 library(reshape2)
 assmatrix = dcast(ass, amphipod~host, length)
 rownames(assmatrix) = assmatrix[,1]
+heatmap(as.matrix(assmatrix[,2:94]), col=c("white", "orange"), Rowv=NA, Colv=NA)
 
 treespp2013 = as.vector(read.table("Hurt2013spp.txt", sep = ",")[,1])
 prunedmatrix = assmatrix[which(assmatrix$amphipod %in% treespp2013),]
