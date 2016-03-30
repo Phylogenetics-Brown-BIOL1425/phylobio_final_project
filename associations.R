85d562ee404a3b6689977d9e3b54a83d390abd72
Alejandro Damian Serrano <athorybia@Alejandros-MacBook-Pro.local>
2016-03-30 19:36:11 -0400

    pruned association matrices in R

--- associations.R
+++ associations.R
@@ -39,7 +39,14 @@ plot(ultram)
 #reprune the matrix
 library(stringr)
 prunedass[,2] = as.matrix(str_replace_all(prunedass[,2],'conica','sp.'))
-reprunedass = as.matrix(prunedass[which(prunedass[,2] %in% JC$tip.label),])
+reprunedass = as.matrix(prunedass[which(prunedass[,2] %in% ultram$tip.label),])
+reprunedass = reprunedass[match(ultram$tip.label, reprunedass[,2]),]
+reprunedassmatrix = as.matrix(dcast(as.data.frame(reprunedass), amphipod~host, length))
+xy = reprunedassmatrix
+reprunedassmatrix = reprunedassmatrix[,2:16]
+class(reprunedassmatrix) <- "numeric"
+rownames(reprunedassmatrix) = dcast(as.data.frame(reprunedass), amphipod~host, length)[,1]
+heatmap(reprunedassmatrix[,2:9], col=c("white", "orange"), Rowv=NA, Colv=NA)
 
 #define association network
 g <- graph_from_edgelist(as.matrix(ass))
@@ -49,21 +56,30 @@ g <- graph_from_edgelist(as.matrix(reprunedass))
 #group vertex color, label color, and vertex location by type
 V(g)$color <- ifelse(V(g)$name %in% hosts, "lightblue", "red")
 V(g)$label.color <- ifelse(V(g)$name %in% hosts, "blue", "darkred")
-V(g)$types <- ifelse(V(g)$name %in% hosts, T, F)
+V(g)$types <- ifelse(V(g)$name %in% hosts, F, T)
 
 #define layouts
 Lo = layout_as_bipartite(g, types = V(g)$types, hgap = 15, vgap = 600)
 Lc = layout_in_circle(g, order = c(V(g)[which(V(g)$name %in% amphipods)], V(g)[which(V(g)$name %in% hosts)]))
 
 #Circle layout
-c= tkplot(g, canvas.width = 700, canvas.height = 700, vertex.size = 2.8, layout=Lc, vertex.label.dist = 1)
+c= tkplot(g, canvas.width = 700, canvas.height = 700, vertex.size = 2.8, layout=Lc, vertex.label.dist = 0.3, vertex.label.cex = 0.5)
 tkplot.fit.to.screen(c, height = 500, width = 500)
 tkplot.center(c)
 #Bipartite layout
-a = tkplot(g, canvas.width = 1200, canvas.height = 900, vertex.size = 2.8, layout=Lo, vertex.label.dist = 1)
+a = tkplot(g, canvas.width = 700, canvas.height = 700, vertex.size = 2.8, layout=Lo, vertex.label.dist = 0.3, vertex.label.cex = 0.5)
 tkplot.rotate(a, degree = 90)
 tkplot.fit.to.screen(a, height = 500, width = 500)
 tkplot.center(a)
 
+#plot phylogentic displays
+ggtree(ultram, layout="circular") + geom_tiplab(aes(angle=angle), color='blue')
+labeled = ultram
+labeled$edge.length = labeled$edge.length * 10
+dd = data.frame(taxa  = labeled$tip.label, amphipod = reprunedass[,1])
+p = ggtree(labeled, layout="rectangular") + xlim(NA, 23)
+p = p %<+% dd + geom_tiplab(aes(color=amphipod), show_guide = FALSE, size = 3.5) + geom_tippoint(aes(color=amphipod), alpha=0.25)
+p+theme(legend.position="right") + guides(color=guide_legend(title="Amphipod", reverse=T)) 
 
-
+yx = ggtree(ultram) + geom_tiplab(color='blue')
+gheatmap(yx, reprunedassmatrix)
