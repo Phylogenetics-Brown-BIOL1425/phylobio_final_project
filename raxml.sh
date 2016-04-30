77a4eb663a46f880b959e6016c772d5472326ce3
DanielSeidman <architeuthis.boy@gmail.com>
2016-04-30 02:47:11 -0400

    First tree built.

---
+++ raxml.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Request an hour of runtime:
+#SBATCH --time=24:00:00
+
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=8
+
+# Specify a job name:
+#SBATCH -J MyMPIJob
+
+# Specify an output file
+#SBATCH -o MyMPIJob-%j.out
+#SBATCH -e MyMPIJob-%j.out
+
+# Run a command
+
+module load raxml
+
+mpirun -n 8 raxmlHPC-MPI -b 12345 -f a -x 12345 -p 12345 -N 100 -m GTRGAMMA -s renamed_all_aligned_sequences.phy -n rax_output
+
